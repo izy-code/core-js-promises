b5b3bf62dcf986601ab68de014ba5e018d1c733e
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-04 14:41:25 +0300

    feat: add 2 solutions

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -91,8 +91,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
@@ -107,8 +107,12 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((settledResults) =>
+    settledResults.map((outcomeObject) =>
+      outcomeObject.status === 'fulfilled' ? outcomeObject.value : null
+    )
+  );
 }
 
 /**
